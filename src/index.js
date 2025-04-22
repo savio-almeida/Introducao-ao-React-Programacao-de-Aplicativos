c30b6b0bac1bc786014539b920dae4ac583d2d60
savio-almeida <142703071+savio-almeida@users.noreply.github.com>
2025-04-22 13:23:21 -0300

    Delete src directory

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import ReactDom from 'react-dom/client';
-
-function App() {
-    return <h1>Rick and Morty</h1>;
-}
-
-const container = document.getElementById('app');
-const root = ReactDOM.createRoot(container);
-root.render(<App />);
