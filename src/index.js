9ad1706444268486c3f2fd9a25f3f52bee7601b6
savio-almeida <142703071+savio-almeida@users.noreply.github.com>
2025-04-22 13:26:40 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom/client';
+import List from './components/List';
+
+function App() {
+    return (
+    <div>
+        <h1>Rick and Morty</h1>
+        <List />
+    </div>
+    );
+}
+
+const container = document.getElementById('app');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
